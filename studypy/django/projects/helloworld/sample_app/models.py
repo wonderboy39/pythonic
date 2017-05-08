909612920fb4712ba190db7df469e1eb73243bfe
wonderboy <wonderboy39@outlook.com>
2017-05-08 22:41:03 +0900

    django Youtube url project  - django YOUTUBE url sample 프로젝트  - models.py의 VideoUrl 클래스에 description필드 추가

--- studypy/django/projects/helloworld/sample_app/models.py
+++ studypy/django/projects/helloworld/sample_app/models.py
@@ -14,6 +14,7 @@ class VideoUrl(models.Model):
     category = models.ForeignKey(VideoCategory)
     subject = models.CharField(max_length=50)
     url = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
     #pub_date = models.DateTimeField('date published') # 추후 다시 추가
     def __unicode__(self):
         return self.subject
