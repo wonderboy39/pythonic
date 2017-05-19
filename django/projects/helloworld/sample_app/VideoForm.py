b14d6d132040ea4d4b4c82b7bd3c50a09a5c852c
wonderboy <showmethecode@naver.com>
2017-05-19 10:25:02 +0900

    django youtube url scrapping APP  - Form 클래스 적용 안되던 문제 해결   원인)   1 - ModelForm클래스르 import한 후에 VideoForm클래스가 ModelForm클래스를 상속받아   해결했으면 풀리는 문제였다   2 - fields에 들어가는 항목중 comment항목이 잘못됨.   데이터베이스의 필드명이 comment가 아닌 description이었는데 혼동하고 잘못 입력함

--- django/projects/helloworld/sample_app/VideoForm.py
+++ django/projects/helloworld/sample_app/VideoForm.py
@@ -1,9 +1,11 @@
 from django import forms
 from .models import VideoUrl
-class VideoForm(forms.Form):
+from django.forms import ModelForm
+# class VideoForm(forms.Form):
+class VideoForm(ModelForm):
     class Meta:
         model = VideoUrl
-        fields = ('subject', 'url', 'comment', 'vod_id',)
+        fields = ('subject', 'url', 'description', 'vod_id',)
         # video_subject = forms.CharField(label='VOD Name', max_length=100)
         # video_url = forms.CharField(label='url', max_length=500)
         # video_comment = forms.CharField(label='comment', max_length=500)
